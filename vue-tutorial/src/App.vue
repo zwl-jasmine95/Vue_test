4f63cad369a0743db1e22c402af7ce1c68b5f200
曾文莉 <zengwenli@emmaclub.com>
2017-08-25 18:40:50 +0800

    vue2.0构建单页应用

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+  <div id="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <i class="glyphicon glyphicon-time"></i>
+          计划板
+        </a>
+        <ul class="nav navbar-nav">
+          <li><router-link to="/home">首页</router-link></li>
+          <li><router-link to="/time-entries">计划列表</router-link></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="col-sm-3">
+
+      </div>
+      <div class="col-sm-9">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import Hello from './components/Hello'
-
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- vue-tutorial/src/main.js
+++ vue-tutorial/src/main.js
@@ -1,13 +1,33 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
 import App from './App'
+import Home from './components/Home'
+import 'bootstrap/dist/css/bootstrap.css'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)   //通过全局方法 Vue.use() 使用插件
+Vue.use(VueResource)
 
-/* eslint-disable no-new */
-new Vue({
+const routes = [{
+  path:'/',
+  component:Home
+},{
+  path:'/home',
+  component:Home
+}]
+
+const router = new VueRouter({
+  routes
+});
+
+var app = new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  router,
+  ...App
+});
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// })
